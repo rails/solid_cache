ef3d36997a2dd02026b1a5d29b43035d3327ba67
Donal McBreen <donal@basecamp.com>
2022-11-18 10:23:31 +0000

    Strip out zeitwerk gem loading

--- lib/active_support/database_cache/engine.rb
+++ lib/active_support/database_cache/engine.rb
@@ -1,12 +1,6 @@
 require "active_support"
 require "active_support/cache"
 require "active_support/database_cache/version"
-require "zeitwerk"
-
-loader = Zeitwerk::Loader.new
-loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
-loader.push_dir(File.expand_path("../..", __dir__))
-loader.setup
 
 module ActiveSupport
   module DatabaseCache
