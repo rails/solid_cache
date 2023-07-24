704237516186ba8ce744ef4c067b55f449661f4b
Donal McBreen <donal@basecamp.com>
2023-07-24 13:17:41 +0100

    Combine migrations into a single file

---
+++ db/migrate/20230724121448_create_solid_cache_entries.rb
@@ -0,0 +1,11 @@
+class CreateSolidCacheEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :solid_cache_entries do |t|
+      t.binary   :key,        null: false,   limit: 1024
+      t.binary   :value,      null: false,   limit: 512.megabytes
+      t.datetime :created_at, null: false
+
+      t.index    :key,        unique: true
+    end
+  end
+end
