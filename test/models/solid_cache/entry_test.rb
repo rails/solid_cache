defb13fc97c957a69f2a2ce4342cbfee17f1f45f
Donal McBreen <donal@basecamp.com>
2023-09-14 11:53:06 +0100

    Use pre-built SQL queries (#69)
    For `Entry.get` and `Entry.get_all`, let's use pre-build SQL queries and
    run them with `find_by_sql` to avoid the cost of ActiveRecord query
    parsing.

--- test/models/solid_cache/entry_test.rb
+++ test/models/solid_cache/entry_test.rb
@@ -7,6 +7,11 @@ module SolidCache
       assert_equal "there", Entry.get("hello".b)
     end
 
+    test "set all and get all cache entries" do
+      Entry.set_all([ { key: "hello".b, value: "there" }, { key: "foo".b, value: "bar" } ])
+      assert_equal({ "foo" => "bar", "hello" => "there" } , Entry.get_all(["hello".b, "foo".b, "bar".b]))
+    end
+
     test "id range" do
       assert_equal 0, Entry.id_range
 
