66be06661c7a656077f7e62e0ab34ff722a2f538
Donal McBreen <donal@basecamp.com>
2023-09-25 12:33:06 +0100

    Clean up between tests manually
    DatabaseCleaner's multi database handling is tricky and all we need
    to do anyway is clear out one table so lets do it ourselves.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,6 @@ ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __d
 require "rails/test_help"
 require "debug"
 require "mocha/minitest"
-require "database_cleaner/active_record"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
@@ -19,17 +18,11 @@ end
 
 ActiveSupport::TestCase.use_transactional_tests = false
 
-DatabaseCleaner.strategy = :truncation
-DatabaseCleaner::ActiveRecord.config_file_location = Rails.root.join("config/database.yml")
-
-DatabaseCleaner[:active_record, db: :primary_shard_one].strategy = :truncation
-DatabaseCleaner[:active_record, db: :primary_shard_two].strategy = :truncation
-DatabaseCleaner[:active_record, db: :secondary_shard_one].strategy = :truncation
-DatabaseCleaner[:active_record, db: :secondary_shard_two].strategy = :truncation
-
 class ActiveSupport::TestCase
   setup do
-    DatabaseCleaner.clean
+    SolidCache.each_shard do
+      SolidCache::Entry.destroy_all
+    end
   end
 end
 
@@ -41,8 +34,10 @@ end
 
 def send_entries_back_in_time(distance)
   @cache.primary_cluster.with_each_shard do
-    SolidCache::Entry.all.each do |entry|
-      entry.update_columns(created_at: entry.created_at - distance)
+    SolidCache::Entry.uncached do
+      SolidCache::Entry.all.each do |entry|
+        entry.update_columns(created_at: entry.created_at - distance)
+      end
     end
   end
 end
