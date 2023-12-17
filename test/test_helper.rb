e6e066d3e684b1dea59a3ddd94bb3955d8a1ee1d
Matt Almeida <matt7@hey.com>
2023-12-17 17:58:13 -0500

    Style

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,7 @@ require "mocha/minitest"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
-  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
+  ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
   ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_paths.first + "/files"
   ActiveSupport::TestCase.fixtures :all
