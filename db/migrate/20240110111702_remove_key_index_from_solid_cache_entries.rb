6b5d0cb469ef74168ee134264050cd778ac8053f
Robert Keresnyei <rk@nemron.com>
2024-01-27 18:09:02 +0100

    Fix RemoveKeyIndexFromSolidCacheEntries migration

--- db/migrate/20240110111702_remove_key_index_from_solid_cache_entries.rb
+++ db/migrate/20240110111702_remove_key_index_from_solid_cache_entries.rb
@@ -1,7 +1,7 @@
 class RemoveKeyIndexFromSolidCacheEntries < ActiveRecord::Migration[7.1]
   def change
     change_table :solid_cache_entries do |t|
-      t.remove_index :key
+      t.remove_index :key, unique: true
     end
   end
 end
