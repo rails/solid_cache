3d138a678670b971f7078bb9a07d46e9506229c4
Donal McBreen <donal@basecamp.com>
2024-02-28 10:59:41 +0000

    Include config/solid_cache.yml when installing
    Add a default config/solid_cache.yml as part of the installer.

--- test/lib/generators/solid_cache/solid_cache/install_generator_test.rb
+++ test/lib/generators/solid_cache/solid_cache/install_generator_test.rb
@@ -7,7 +7,7 @@ module SolidCache
   class SolidCache::InstallGeneratorTest < Rails::Generators::TestCase
     tests SolidCache::InstallGenerator
 
-    destination File.expand_path("../../../../fixtures/tmp", __dir__)
+    destination File.expand_path("../../../../../tmp", __dir__)
     setup :prepare_destination
 
     setup do
@@ -18,10 +18,32 @@ module SolidCache
 
     test "generator updates environment config" do
       run_generator [ "--skip-migrations" ]
+      assert_file "#{destination_root}/config/solid_cache.yml", expected_config
       assert_file "#{destination_root}/config/environments/development.rb", /config.cache_store = :solid_cache_store\n/
       assert_file "#{destination_root}/config/environments/development.rb", /config.cache_store = :null_store\n/
       assert_file "#{destination_root}/config/environments/test.rb", /config.cache_store = :null_store\n/
       assert_file "#{destination_root}/config/environments/production.rb", /config.cache_store = :solid_cache_store\n/
     end
+
+    private
+      def expected_config
+        <<~YAML
+          default: &default
+            database: <%= Rails.env %>
+            store_options:
+              max_age: <%= 1.week.to_i %>
+              max_size: <%= 256.megabytes %>
+              namespace: <%= Rails.env %>
+
+          development:
+            <<: *default
+
+          test:
+            <<: *default
+
+          production:
+            <<: *default
+        YAML
+      end
   end
 end
