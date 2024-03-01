c7728652bf5b76824dd835db251423d16322cec8
Donal McBreen <donal@basecamp.com>
2024-03-01 11:31:13 +0000

    Update cache behaviour tests

--- test/unit/behaviors/failure_safety_behavior.rb
+++ test/unit/behaviors/failure_safety_behavior.rb
@@ -49,10 +49,10 @@ module FailureSafetyBehavior
     end
   end
 
-  def test_write_failure_returns_false
+  def test_write_failure_returns_nil
     key = SecureRandom.uuid
     emulating_unavailability do |cache|
-      assert_equal false, cache.write(key, SecureRandom.alphanumeric)
+      assert_nil cache.write(key, SecureRandom.alphanumeric)
     end
   end
 
@@ -91,6 +91,19 @@ module FailureSafetyBehavior
     end
   end
 
+  def test_delete_multi_failure_returns_zero
+    key = SecureRandom.uuid
+    other_key = SecureRandom.uuid
+    @cache.write_multi(
+      key => SecureRandom.alphanumeric,
+      other_key => SecureRandom.alphanumeric
+    )
+
+    emulating_unavailability do |cache|
+      assert_equal 0, cache.delete_multi([key, other_key])
+    end
+  end
+
   def test_exist_failure_returns_false
     key = SecureRandom.uuid
     @cache.write(key, SecureRandom.alphanumeric)
@@ -115,6 +128,12 @@ module FailureSafetyBehavior
 
     emulating_unavailability do |cache|
       assert_nil cache.decrement(key)
+    end
+  end
+
+  def test_clear_failure_returns_nil
+    emulating_unavailability do |cache|
+      assert_nil cache.clear
     end
   end
 end
