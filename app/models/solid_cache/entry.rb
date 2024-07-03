820f8ddbf6d9183fc25ffab40c8c08776aaaa642
Oliver Morgan <ollym@me.com>
2024-07-03 17:15:39 +0100

    all the way back

--- app/models/solid_cache/entry.rb
+++ app/models/solid_cache/entry.rb
@@ -134,7 +134,7 @@ module SolidCache
             if connection.prepared_statements?
               result = connection.select_all(sanitize_sql(query), "#{name} Load", Array(values), preparable: true)
             else
-              result = connection.select_all(sanitize_sql([ query, Array(values) ]), "#{name} Load", Array(values), preparable: false)
+              result = connection.select_all(sanitize_sql([ query, values ]), "#{name} Load", binds, preparable: false)
             end
 
             result.cast_values(SolidCache::Entry.attribute_types)
