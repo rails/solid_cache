fb7debdca9384db69f988fb91053dfd8c2f54657
Orban Botond <orbanbotond@gmail.com>
2025-11-06 17:49:48 +0200

    Generate reference to the database in the cache template for all envi… (#279)
    * Generate reference to the database in the cache template for all environments.
    
    * Set database once in default
    
    ---------
    
    Co-authored-by: Donal McBreen <donal@37signals.com>

--- test/lib/generators/solid_cache/solid_cache/install_generator_test.rb
+++ test/lib/generators/solid_cache/solid_cache/install_generator_test.rb
@@ -30,6 +30,7 @@ module SolidCache
       def expected_cache_config
         <<~YAML
           default: &default
+            database: cache
             store_options:
               # Cap age of oldest cache entry to fulfill retention policies
               # max_age: <%= 60.days.to_i %>
@@ -43,7 +44,6 @@ module SolidCache
             <<: *default
 
           production:
-            database: cache
             <<: *default
         YAML
       end
